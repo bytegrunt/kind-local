c81feb400e8fcd94551439c7183fdabe70fbc546
bytegrunt <207869117+bytegrunt@users.noreply.github.com>
2025-05-17 00:37:29 -0400

    add argocd

--- 0_install.sh
+++ 0_install.sh
@@ -20,5 +20,17 @@ helm upgrade --install ingress-nginx ingress-nginx \
   --values ./setup/helm/ingress-nginx.yaml \
   --kubeconfig "$kubeconfig_docker"
 
+kubectl apply -f ./setup/argocd/install.yaml
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n default --timeout=120s
+kubectl apply -f ./setup/argocd/ingress.yaml
+
 echo ""
 echo ">>>> Everything prepared, ready to deploy application."
+echo ">>>>"
+echo ">>>> To access ArgoCD UI, use the following command:"
+echo ">>>> https://localhost:30443"
+echo ">>>> username: admin"
+echo ">>>> password: $(kubectl get secret argocd-initial-admin-secret -n default -o jsonpath='{.data.password}' | base64 --decode)"
+
+
+
